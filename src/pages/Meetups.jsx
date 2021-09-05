99d0d091c295581b2a50bbd266bd04477c922576
Rafael Ribeiro <cortadordeunha666@gmail.com>
2021-09-05 16:30:13 -0300

    refactor: extended props destructuring assignment.

--- src/pages/Meetups.jsx
+++ src/pages/Meetups.jsx
@@ -5,7 +5,7 @@ import Card from '../components/Card';
 import CreateMeetup from '../components/CreateMeetup'
 
 function Meetups(props) {
-  const {changeUnderlinedAnchor} = props
+  const {changeUnderlinedAnchor, modalState, changeModalState} = props
   
   const DUMMY_DATA = [
     {
@@ -72,7 +72,11 @@ function Meetups(props) {
     <Fragment>
       <h1>Encontros</h1>
 
-      <button className={styleClasses.meetupCreatorButton}>
+      <button 
+        onClick={() => changeModalState(!modalState)}
+        className={styleClasses.meetupCreatorButton}
+        aria-label="abrir janela de criação de encontro"
+      >
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>
       </button>
 
