182ebca98359225511357658ec4a5b17d1c7df38
Rafael Ribeiro <cortadordeunha666@gmail.com>
2021-09-11 15:54:10 -0300

    feat: implemented firebase's firestore database system and imported the meetupCreatorModal component.

--- src/pages/Meetups.jsx
+++ src/pages/Meetups.jsx
@@ -1,55 +1,52 @@
-import {Fragment} from 'react'
+import {Fragment, useState, useEffect} from 'react';
+
+import firestoreDatabase from '../firestore';
+import { collection, getDocs } from "firebase/firestore";
 
 import styleClasses from '../scss/Meetups.module.scss';
 import Card from '../components/Card';
-import CreateMeetup from '../components/CreateMeetup'
+import CreateMeetup from '../components/CreateMeetup';
+import MeetupCreatorModal from '../components/MeetupCreatorModal';
 
 function Meetups(props) {
+  const [meetupsState, changeMeetupsState] = useState()
+  const [dataIsLoading, changeDataIsLoadingState] = useState(true)
   const {changeUnderlinedAnchor, modalState, changeModalState} = props
+  let cardImageIndex;   // it will help to define the images that must get resized
   
-  const DUMMY_DATA = [
-    {
-      title: 'This is a first meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-      address: 'Meetupstreet 5, 12345 Meetup City',
-      hour: '09:35',
-      date: '03/21/31'
-    },
-    {
-      title: 'This is a second meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-      address: 'Meetupstreet 5, 12345 Meetup City',
-      date: '03/21/31'
-    },
-    {
-      title: 'Second meetup',
-      image:
-        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.XwvslP5BMK3QnIyqwK2C1QHaE0%26pid%3DApi&f=1',
-      description: 'Amazing meetup!',
-      address: 'Rua Pedro Paulo, 120',
-      hour: '09:35',
-      date: '03/21/31'
-    }
-  ];
-
   changeUnderlinedAnchor('first-anchor')
 
+  useEffect(getMeetupsData, [])
+  
+  function getMeetupsData() {
+    getDocs(collection(firestoreDatabase, "meetups"))
+    .then(returnedData => {
+      const parsedData = returnedData.docs.map(doc => doc.data())
+
+      changeMeetupsState(parsedData)
+      changeDataIsLoadingState(false)
+    })
+    .catch(error => {
+      window.alert('Ocorreu um erro. Reinicie a página.')
+    })
+  }
+
   function returnCards()  {
+    cardImageIndex = -1
+
     return (
       <div className={styleClasses.cardsGridContainer}>
         {
-          DUMMY_DATA.map((data, index) => {
+          meetupsState.map((data, index) => {
+            if (data.image) {
+              ++cardImageIndex
+            }
+            
             return (
               <Card
-                cardIndex={index}
-                titleName={data.title}
+                cardIndex={cardImageIndex}
                 imageSource={data.image}
+                titleName={data.title}
                 descriptionText={data.description}
                 address={data.address}
                 hour={data.hour}
@@ -61,18 +58,17 @@ function Meetups(props) {
         }
       </div>
     )
-    
   }
 
   function createCards() {
-    return DUMMY_DATA.length ? returnCards() : <CreateMeetup />
+    return meetupsState.length ? returnCards() : <CreateMeetup modalState={modalState} changeModalState={changeModalState} />
   }
 
   return (
     <Fragment>
       <h1>Encontros</h1>
 
-      <button 
+      <button
         onClick={() => changeModalState(!modalState)}
         className={styleClasses.meetupCreatorButton}
         aria-label="abrir janela de criação de encontro"
@@ -80,7 +76,12 @@ function Meetups(props) {
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>
       </button>
 
-      {createCards()}
+      {!dataIsLoading ? createCards() : null}
+
+      <MeetupCreatorModal 
+        modalState={modalState} changeModalState={changeModalState} 
+        meetupsState={meetupsState} changeMeetupsState={changeMeetupsState}
+      />
     </Fragment>
   )
 }
