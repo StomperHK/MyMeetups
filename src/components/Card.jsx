e3fc3da7d5d958a7643b38426fa6ec2a7e2228a2
Rafael Ribeiro <cortadordeunha666@gmail.com>
2021-09-11 15:41:19 -0300

    feat: implemented card's height change when it doesn't has a image feature and changed style's classes.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,12 @@
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
 
 import styleClasses from '../scss/Card.module.scss'
 
 function Card(props) {
+  const [imageState, changeImageState] = useState('empty')
   const {cardIndex, titleName, imageSource, descriptionText, address, hour, date} = props
+
+  useEffect(defineImagesSizes, [])
 
   function defineImagesSizes() {
     try {
@@ -15,12 +18,16 @@ function Card(props) {
     }
   }
 
-  useEffect(defineImagesSizes, [])
-
   return (
-    <article className={styleClasses.card}>
+    <article 
+      className={`${styleClasses.card} ${'empty error'.includes(imageState) ? 'fit-content-height' : ''}`}
+    >
       <figure>
-        {imageSource ? <img className='card-image' src={imageSource} alt="" /> : null}
+        {
+          imageSource ? 
+          <img className='card-image' onLoad={() => changeImageState('loaded')} onError={() => changeImageState('error')} src={imageSource} alt="" /> :
+          null
+        }
 
         <figcaption>
           <h2>{titleName}</h2>
@@ -37,7 +44,7 @@ function Card(props) {
             </li>
             <li>
               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V10h14v10zm0-12H5V6h14v2zm-7 5h5v5h-5z"/></svg>
-              <p>09/03/2021</p>
+              <p>{date}</p>
             </li>
           </ul>
         </figcaption>
@@ -47,7 +54,7 @@ function Card(props) {
         <button aria-label="excluir encontro">
            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/></svg>
         </button>
-        <button>
+        <button className="generic-button">
           ver mais
         </button>
         <button aria-label="favoritar encontro">
