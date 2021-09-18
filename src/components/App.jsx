85f7a1f1188a7342d776358b6b4d0f43d8ca39a9
Rafael Ribeiro <cortadordeunha666@gmail.com>
2021-09-18 10:48:34 -0300

    feat: imported LoadingScreen and ConfirmModal components and created it's states; refactor: edited element's props.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,22 @@ import {Fragment, useState, useEffect} from 'react';
 import {Route, Switch} from 'react-router-dom';
 
 import firestoreDatabase from '../firestore';
-import { collection, getDocs } from "firebase/firestore";
+import {getDocs, collection} from "firebase/firestore";
 
 import styleClasses from '../scss/App.module.scss';
 import MainHeader from './MainHeader';
 import Meetups from '../pages/Meetups';
 import Bookmarks from '../pages/Bookmarks';
+import LoadingScreen from './LoadingScreen';
+import ConfirmModal from './ConfirmModal';
 import FeedbackModal from './FeedbackModal';
 import MeetupCreatorModal from '../components/MeetupCreatorModal';
 
 function App() {
   const [anchorToGetUnderlinedState, changeAnchorToGetUnderlinedState] = useState('')
-  const [meetupsState, changeMeetupsState] = useState([])
-  const [dataIsLoading, changeDataIsLoadingState] = useState(false)
+  const [meetupsState, changeMeetupsState] = useState()
+  const [dataIsLoading, changeDataIsLoadingState] = useState(true)
+  const [confirmModalState, changeConfirmModalState] = useState([false, ''])
   const [feedbackModalState, changeFeedbackModalState] = useState([false, '_', 'normal'])
   const [meetupCreatorModalState, changeMeetupCreatorModalState] = useState(false)
 
@@ -23,13 +26,17 @@ function App() {
   function getMeetupsData() {
     getDocs(collection(firestoreDatabase, "meetups"))
     .then(returnedData => {
-      const parsedData = returnedData.docs.map(doc => doc.data())
+      const parsedData = returnedData.docs.map(doc => {
+        let temporaryObject = doc.data()
+        temporaryObject.id = doc.id
+        return temporaryObject
+      })
 
       changeMeetupsState(parsedData)
       changeDataIsLoadingState(false)
     })
-    .catch(error => {
-      changeFeedbackModalState([true, 'erro ao obter dados. reinicie a página', 'error'])
+    .catch(() => {
+      changeFeedbackModalState([true, 'erro ao obter dados: reinicie a página', 'error', 3000])
     })
   }
 
@@ -46,16 +53,28 @@ function App() {
           <Route path='/' exact >
             <Meetups
               meetupsState={meetupsState}
-              dataIsLoading={dataIsLoading} 
-              changeDataIsLoadingState={changeDataIsLoadingState}
-              changeAnchorToGetUnderlinedState={changeAnchorToGetUnderlinedState} 
-              changeMeetupCreatorModalState={changeMeetupCreatorModalState} 
+              changeMeetupsState={changeMeetupsState}
+              dataIsLoading={dataIsLoading}
+              changeAnchorToGetUnderlinedState={changeAnchorToGetUnderlinedState}
+              changeFeedbackModalState={changeFeedbackModalState}
+              confirmModalState={confirmModalState}
+              changeConfirmModalState={changeConfirmModalState}
               meetupCreatorModalState={meetupCreatorModalState}
+              changeMeetupCreatorModalState={changeMeetupCreatorModalState}
             />
           </Route>
           
           <Route path="/bookmarks">
-            <Bookmarks changeAnchorToGetUnderlinedState={changeAnchorToGetUnderlinedState} />
+            <Bookmarks
+              meetupsState={meetupsState}
+              changeMeetupsState={changeMeetupsState}
+              dataIsLoading={dataIsLoading}
+              changeAnchorToGetUnderlinedState={changeAnchorToGetUnderlinedState}
+              confirmModalState={confirmModalState}
+              changeConfirmModalState={changeConfirmModalState}
+              meetupCreatorModalState={meetupCreatorModalState}
+              changeMeetupCreatorModalState={changeMeetupCreatorModalState}
+            />
           </Route>
         </Switch>
 
@@ -67,16 +86,23 @@ function App() {
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>
         </button>
 
-        <MeetupCreatorModal
-          changeFeedbackModalState={changeFeedbackModalState}
-          meetupCreatorModalState={meetupCreatorModalState} 
-          changeMeetupCreatorModalState={changeMeetupCreatorModalState}
-          meetupsState={meetupsState} changeMeetupsState={changeMeetupsState}
+        {dataIsLoading ? <LoadingScreen /> : null}
+
+        <ConfirmModal 
+          confirmModalState={confirmModalState}
+          changeConfirmModalState={changeConfirmModalState}
         />
 
         <FeedbackModal
           feedbackModalState={feedbackModalState}
           changeFeedbackModalState={changeFeedbackModalState}
+        />
+ 
+        <MeetupCreatorModal
+          changeFeedbackModalState={changeFeedbackModalState}
+          meetupCreatorModalState={meetupCreatorModalState} 
+          changeMeetupCreatorModalState={changeMeetupCreatorModalState}
+          meetupsState={meetupsState} changeMeetupsState={changeMeetupsState}
         />
       </main>
     </Fragment>
