f1881f1ed0bd22924a4b7b4d79827cf9abf7ca49
Rafael Ribeiro <cortadordeunha666@gmail.com>
2021-09-18 13:18:20 -0300

    refactor: turned component operable.

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,11 +1,72 @@
-import {useEffect} from 'react';
+import {Fragment, useEffect} from 'react';
+
+import styleClasses from '../scss/App.module.scss';
+import Card from '../components/Card';
+import CreateMeetup from '../components/CreateMeetup';
 
 function Bookmarks(props) {
-  const {changeAnchorToGetUnderlinedState} = props
+  const {
+    meetupsState,
+    changeMeetupsState,
+    dataIsLoading,
+    changeAnchorToGetUnderlinedState,
+    changeFeedbackModalState,
+    confirmModalState,
+    changeConfirmModalState,
+    meetupCreatorModalState,
+    changeMeetupCreatorModalState
+  } = props
 
   useEffect(() =>changeAnchorToGetUnderlinedState('second-anchor'), [changeAnchorToGetUnderlinedState])
 
-  return <h1>Bookmarks</h1>
+  function knowIfAnyMeetupIsBookmarked() {
+    return meetupsState.some(meetup => meetup.isBookmarked)
+  }
+
+  function returnCards()  {
+    return (
+      <div className={styleClasses.cardsGridContainer}>
+        {
+          meetupsState.reduce((accum, data, index) => {
+            if (data.isBookmarked) {
+              accum.push(
+                <Card
+                  cardIndex={index}
+                  meetupsState={meetupsState}
+                  changeMeetupsState={changeMeetupsState}
+                  changeFeedbackModalState={changeFeedbackModalState}
+                  confirmModalState={confirmModalState}
+                  changeConfirmModalState={changeConfirmModalState}
+                  imageSource={data.image}
+                  titleName={data.title}
+                  descriptionText={data.description}
+                  address={data.address}
+                  hour={data.hour}
+                  date={data.date}
+                  isBookmarked={data.isBookmarked}
+                  key={index}
+                />
+              )
+            }
+            
+            return accum
+          }, [])
+        }
+      </div>
+    )
+  }
+
+  function createCards() {
+    return knowIfAnyMeetupIsBookmarked() ? returnCards() : <CreateMeetup meetupCreatorModalState={meetupCreatorModalState} changeMeetupCreatorModalState={changeMeetupCreatorModalState} />
+  }
+
+  return (
+    <Fragment>
+      <h1>Favoritos</h1>
+
+      {!dataIsLoading && meetupsState ? createCards() : null}
+    </Fragment>
+  )
 }
 
 export default Bookmarks;
