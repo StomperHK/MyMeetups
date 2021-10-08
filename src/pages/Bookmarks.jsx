9c33c2b44e50ffaf7b0b9c7979700dca768d37fe
Rafael Ribeiro <cortadordeunha666@gmail.com>
2021-10-08 20:53:12 -0300

    refactor: made changes to reflect application's changes.

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,19 +1,23 @@
-import {Fragment, useEffect} from 'react';
+import {useEffect} from 'react';
 
-import styleClasses from '../scss/App.module.scss';
+import {Link} from 'react-router-dom';
+
+import styleClasses from '../scss/ApplicationCore.module.scss';
 import Card from '../components/Card';
-import ReturnToMeetups from '../components/ReturnToMeetups';
+import AlternativeAction from '../components/AlternativeAction';
 
 function Bookmarks(props) {
   const {
     meetupsState,
     changeMeetupsState,
     dataIsLoading,
+    changeDataIsLoadingState,
     changeAnchorToGetUnderlinedState,
     changeFeedbackModalState,
     confirmModalState,
     changeConfirmModalState,
     changeMeetupViewerState,
+    currentUser
   } = props
 
   useEffect(() =>changeAnchorToGetUnderlinedState('second-anchor'), [changeAnchorToGetUnderlinedState])
@@ -38,10 +42,12 @@ function Bookmarks(props) {
                   cardIndex={index}
                   meetupsState={meetupsState}
                   changeMeetupsState={changeMeetupsState}
+                  changeDataIsLoadingState={changeDataIsLoadingState}
                   changeFeedbackModalState={changeFeedbackModalState}
                   confirmModalState={confirmModalState}
                   changeConfirmModalState={changeConfirmModalState}
                   changeMeetupViewerState={changeMeetupViewerState}
+                  currentUser={currentUser}
                   imageSource={data.image}
                   titleName={data.title}
                   address={data.address}
@@ -60,18 +66,31 @@ function Bookmarks(props) {
   }
 
   function createCards() {
-    return knowIfAnyMeetupIsBookmarked() ? returnCards() : <ReturnToMeetups />
+    return knowIfAnyMeetupIsBookmarked() ?
+    returnCards() :
+    (
+      <AlternativeAction>
+        <Link className='createMeetupWrapper' to="/encontros" aria-label="ir para encontros">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42-.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1z"/></svg>
+          </div>
+          <p>
+            ir para encontros
+          </p>
+        </Link>
+      </AlternativeAction>
+    )
   }
 
   return (
-    <Fragment>
+    <article>
       <div className={`introduction-wrapper ${!bookmarkedMeetups ? 'reduce-padding' : ''}`}>
         <h1>Favoritos</h1>
         <p>{bookmarkedMeetups} favorito{bookmarkedMeetups === 1 ? '' : 's'}</p>
       </div>
 
       {!dataIsLoading && meetupsState ? createCards() : null}
-    </Fragment>
+    </article>
   )
 }
 
--- src/pages/Meetups.jsx
+++ src/pages/Meetups.jsx
@@ -1,21 +1,23 @@
-import {Fragment, useEffect} from 'react';
+import {useEffect} from 'react';
 
-import styleClasses from '../scss/App.module.scss';
+import styleClasses from '../scss/ApplicationCore.module.scss';
 import Card from '../components/Card';
-import CreateMeetup from '../components/CreateMeetup';
+import AlternativeAction from '../components/AlternativeAction';
 
 function Meetups(props) {
   const {
     meetupsState,
     changeMeetupsState,
     dataIsLoading,
+    changeDataIsLoadingState,
     changeAnchorToGetUnderlinedState,
     changeFeedbackModalState,
     confirmModalState,
     changeConfirmModalState,
     meetupCreatorModalState,
     changeMeetupViewerState,
-    changeMeetupCreatorModalState
+    changeMeetupCreatorModalState,
+    currentUser
   } = props
   
   useEffect(() => changeAnchorToGetUnderlinedState('first-anchor'), [changeAnchorToGetUnderlinedState])
@@ -29,10 +31,12 @@ function Meetups(props) {
               cardIndex={index}
               meetupsState={meetupsState}
               changeMeetupsState={changeMeetupsState}
+              changeDataIsLoadingState={changeDataIsLoadingState}
               changeFeedbackModalState={changeFeedbackModalState}
               confirmModalState={confirmModalState}
               changeConfirmModalState={changeConfirmModalState}
               changeMeetupViewerState={changeMeetupViewerState}
+              currentUser={currentUser}
               imageSource={data.image}
               titleName={data.title}
               address={data.address}
@@ -48,18 +52,34 @@ function Meetups(props) {
   }
 
   function createCards() {
-    return meetupsState.length ? returnCards() : <CreateMeetup meetupCreatorModalState={meetupCreatorModalState} changeMeetupCreatorModalState={changeMeetupCreatorModalState} />
+    return meetupsState.length ?
+    returnCards() : 
+    (
+      <AlternativeAction>
+        <button className='createMeetupWrapper' 
+          onClick={() => changeMeetupCreatorModalState(!meetupCreatorModalState)}
+          aria-label="criar encontro"
+        >
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+          </div>
+          <p>
+            criar encontro
+          </p>
+        </button>
+      </AlternativeAction>
+    )
   }
 
   return (
-    <Fragment>
+    <article>
       <div className={`introduction-wrapper ${!meetupsState.length ? 'reduce-padding' : ''}`}>
         <h1>Encontros</h1>
         <p>{meetupsState.length} encontro{meetupsState.length === 1 ? '' : 's'}</p>
       </div>
 
       {!dataIsLoading ? createCards() : null}
-    </Fragment>
+    </article>
   )
 }
 
