80d9ed23d9d334cf9229b5c8e7660805b5fc25b5
Rafael Ribeiro <cortadordeunha666@gmail.com>
2021-10-11 20:26:38 -0300

    refactor: changed routes.

--- src/pages/ApplicationCore.jsx
+++ src/pages/ApplicationCore.jsx
@@ -18,7 +18,7 @@ import MeetupViewer from '../components/MeetupViewer';
 import MeetupCreatorModal from '../components/MeetupCreatorModal';
 
 function ApplicationCore(props) {
-  useEffect(() => changeDataIsLoadingState(true), []) // keep it truthy
+  useEffect(() => changeDataIsLoadingState(true), [])
 
   const [anchorToGetUnderlinedState, changeAnchorToGetUnderlinedState] = useState('')
   const [meetupsState, changeMeetupsState] = useState([])
@@ -110,7 +110,7 @@ function ApplicationCore(props) {
         changeDataIsLoadingState={changeDataIsLoadingState}
         changeFeedbackModalState={changeFeedbackModalState}
       >
-        <Link to="/encontros">
+        <Link to="/">
           <Button size="medium"
             startIcon={<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H8V4h12v12zM10 9h8v2h-8zm0 3h4v2h-4zm0-6h8v2h-8z"/></svg>}
           >
@@ -129,7 +129,7 @@ function ApplicationCore(props) {
       
       <main>
         <Switch>
-          <Route path='/encontros'>
+          <Route path='/' exact>
             <Meetups
               changeDataIsLoadingState={changeDataIsLoadingState}
               meetupsState={meetupsState}
