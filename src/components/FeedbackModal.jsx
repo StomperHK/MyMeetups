5cc532da4c311570fe651e68e5675be33e6b110c
Rafael Ribeiro <cortadordeunha666@gmail.com>
2021-10-11 20:21:46 -0300

    feat: created close modal funcionality.

--- src/components/FeedbackModal.jsx
+++ src/components/FeedbackModal.jsx
@@ -7,9 +7,10 @@ function FeedbackModal(props) {
 
   let timeoutToCloseModal = useRef()
 
+  const modalOptions = feedbackModalState.slice(1)
+
   useEffect(() => {
     if (feedbackModalState[0]) {
-      const modalOptions = feedbackModalState.slice(1)
       const duration = feedbackModalState[3]
 
       clearTimeout(timeoutToCloseModal.current)
@@ -35,7 +36,12 @@ function FeedbackModal(props) {
         ${getModalState() ? styleClasses.modalActiver : ''}
       `}
     >
-      <span>{feedbackModalState[1]}</span>
+      <p><span>{feedbackModalState[1]}</span></p>
+      <button onClick={() => changeFeedbackModalState([false, ...modalOptions])}
+        aria-label="fechar janela de alerta"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
+      </button>
     </div>
   )
 }
