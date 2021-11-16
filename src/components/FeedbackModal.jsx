ebad84590f40d65e943f9c1ce12fe2f4025ab3a1
Rafael Ribeiro <cortadordeunha666@gmail.com>
2021-11-16 18:56:22 -0300

    refactor: improved component readability and acessibility.

--- src/components/FeedbackModal.jsx
+++ src/components/FeedbackModal.jsx
@@ -7,17 +7,19 @@ function FeedbackModal(props) {
 
   let timeoutToCloseModal = useRef()
 
-  const modalOptions = feedbackModalState.slice(1)
+  const modalRest = feedbackModalState.slice(1)
 
-  useEffect(() => {
+  useEffect(scheduleModalSelfClosing, [feedbackModalState, changeFeedbackModalState, modalRest])
+
+  function scheduleModalSelfClosing() {
     if (feedbackModalState[0]) {
       const duration = feedbackModalState[3]
 
       clearTimeout(timeoutToCloseModal.current)
-      timeoutToCloseModal.current = 
-        setTimeout(() => {changeFeedbackModalState([false, ...modalOptions])}, duration)
+      timeoutToCloseModal.current =
+        setTimeout(() => {changeFeedbackModalState([false, ...modalRest])}, duration)
     }
-  }, [feedbackModalState, changeFeedbackModalState])
+  }
 
   function getModalType() {
     return feedbackModalState[2]
@@ -28,18 +30,19 @@ function FeedbackModal(props) {
   }
 
   return (
-    <div 
+    <div
       className={`
         ${styleClasses.modalContainer}
         ${getModalType() === 'error' ? styleClasses.modalErrorColor : ''}
         ${getModalType() === 'alert' ? styleClasses.modalAlertColor : ''}
         ${getModalState() ? styleClasses.modalActiver : ''}
       `}
-      aria-hidden={getModalState() ? 'undefined' : 'true'}
     >
-      <p><span>{feedbackModalState[1]}</span></p>
-      <button onClick={() => changeFeedbackModalState([false, ...modalOptions])}
-        aria-label="fechar janela de alerta"
+      <p>
+        <span role="alert">{feedbackModalState[1]}</span>
+      </p>
+      <button onClick={() => changeFeedbackModalState([false, ...modalRest])}
+        aria-hidden="true" tabIndex={getModalState() ? "0" : "-1"}
       >
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
       </button>
