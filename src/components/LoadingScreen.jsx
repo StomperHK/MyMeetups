ec3ae0a8f3cc6f3288a51bd0060529739ea057a4
Rafael Ribeiro <cortadordeunha666@gmail.com>
2021-11-16 19:10:41 -0300

    refactor: created hide window scrollbar and animated the component's visibility.

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,10 +1,27 @@
+import {useEffect} from 'react';
+
 import styleClasses from '../scss/LoadingScreen.module.scss'
 
-function LoadingScreen() {
+function LoadingScreen({dataIsLoading}) {
+  useEffect(toggleHideWindowScrollbarWhenLoadingScreenAppears, [dataIsLoading])
+
+  function toggleHideWindowScrollbarWhenLoadingScreenAppears() {
+    const htmlEL = document.querySelector('html')
+
+    if (dataIsLoading) {
+      htmlEL.classList.add('overflow-hidden')
+    }
+    else {
+      htmlEL.classList.remove('overflow-hidden')
+    }
+  }
+
   return (
-    <div className={styleClasses.backdropArea}>
+    <div className={`${styleClasses.backdropArea} ${dataIsLoading ? styleClasses.showLoadingScreen : ''}`}
+      role="progressbar" aria-valuetext="carregando" id="loading-screen"
+    >
       <div className={styleClasses.loadingBarWrapper}>
-        <span></span>
+        <span className={dataIsLoading ? styleClasses.playAnimation : ''}></span>
       </div>
     </div>
   )
